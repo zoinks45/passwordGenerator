22a2b1a86fe0503e5bd490dc7c11c434419167ab
Andre <anm54813@gmail.com>
2022-06-27 17:09:41 -0700

    finished pop ups

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,19 @@
 // Assignment code here
+//character length
+var charlength = Number(prompt("How many characters will your password be? Enter a number between 8 and 128"));
 
+//character type
+var upperChar = confirm("would you like upper case?");
+var numberChar = confirm("would you like numbers?");
+var specialChar = confirm("would you like special characters?")
+
+//generate character codes
+
+
+// build password
+function buildPassword (charlength, upperChar, numberChar, specialChar){
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
